74a64c7b9cabdc23927669a0356cd655ea52e9ce
fishhood <uyenntaoo@gmail.com>
2021-05-18 17:18:42 +0700

    test

---
+++ ShopGiay/Assets/js/car.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+
+        autoHeight: true
+
+    })
+    var owl = $('.owl-carousel owl-theme');
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+
+    })
+})
